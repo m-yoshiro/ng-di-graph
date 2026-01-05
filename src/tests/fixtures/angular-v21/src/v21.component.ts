f63d2b4d8ac0f9635684061c3aa37470c741821e
m-yoshiro <5795937+m-yoshiro@users.noreply.github.com>
2026-01-06 08:00:09 +0900

    test: focus v21 fixture on FormArrayDirective

--- src/tests/fixtures/angular-v21/src/v21.component.ts
+++ src/tests/fixtures/angular-v21/src/v21.component.ts
@@ -1,10 +1,5 @@
-import { Component, Injectable, InjectionToken, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormArrayDirective } from '@angular/forms';
-
-export const V21_TOKEN = new InjectionToken<string>('v21.token');
-
-@Injectable({ providedIn: 'root' })
-export class V21Service {}
 
 @Component({
   selector: 'app-v21',
@@ -13,34 +8,5 @@ export class V21Service {}
   template: '<div></div>',
 })
 export class V21Component {
-  private readonly service = inject(V21Service);
-
   constructor(private readonly formArray: FormArrayDirective) {}
-}
-
-@Component({
-  selector: 'app-v21-no-constructor',
-  standalone: true,
-  template: '<div></div>',
-})
-export class NoConstructorComponent {
-  readonly service = inject(V21Service);
-}
-
-@Component({
-  selector: 'app-v21-inject-options',
-  standalone: true,
-  template: '<div></div>',
-})
-export class InjectWithOptionsComponent {
-  readonly optionalService = inject(V21Service, { optional: true });
-}
-
-@Component({
-  selector: 'app-v21-inject-token',
-  standalone: true,
-  template: '<div></div>',
-})
-export class TokenInjectComponent {
-  readonly tokenValue = inject(V21_TOKEN);
 }
